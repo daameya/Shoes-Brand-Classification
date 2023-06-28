fadfb192c1fdfd1369d093c895e76028fb698b99
Ameya Damle <1998ameya@gmail.com>
2023-06-28 02:02:32 +0100

    data ingestion completed

---
+++ main.py
@@ -0,0 +1,13 @@
+from shoes_brand_classification import logger
+from shoes_brand_classification.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx=======x")
+except Exception as e:
+        logger.exception(e)
+        raise e
