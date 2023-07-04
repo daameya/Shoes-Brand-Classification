972302f90ac68b153058c73c62a1037ccdbcd68c
Ameya Damle <1998ameya@gmail.com>
2023-07-04 17:57:20 +0100

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from shoes_brand_classification import logger
 from shoes_brand_classification.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from shoes_brand_classification.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -7,6 +8,18 @@ try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx=======x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx=======x")
 except Exception as e:
         logger.exception(e)
