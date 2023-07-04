71fd5836eba983674e737c6617687ce79215c6a9
Ameya Damle <1998ameya@gmail.com>
2023-07-04 22:48:43 +0100

    prepare callbacks added

--- src/shoes_brand_classification/config/configuration.py
+++ src/shoes_brand_classification/config/configuration.py
@@ -1,6 +1,8 @@
 from shoes_brand_classification.constants import *
+import os
 from shoes_brand_classification.utils.common import read_yaml, create_directories
-from shoes_brand_classification.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig)
+from shoes_brand_classification.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig,
+                                                             PrepareCallbacksConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -47,3 +49,22 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+
+        return prepare_callback_config
+
