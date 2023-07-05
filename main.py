c6ed30392ed402b9788a8080a5cfbc774a2a859c
Ameya Damle <1998ameya@gmail.com>
2023-07-05 01:44:11 +0100

    model training added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from shoes_brand_classification import logger
 from shoes_brand_classification.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from shoes_brand_classification.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from shoes_brand_classification.pipeline.stage03_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -21,6 +23,20 @@ try:
     obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx=======x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"************************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx===========x")
 except Exception as e:
         logger.exception(e)
         raise e
